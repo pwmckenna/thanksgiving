305e6fd7c2a15cef00bd0c9a044629d390be0ceb
Patrick Williams <pwmckenna@gmail.com>
2015-11-24 11:19:40 -0800

    production build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,8 +4,6 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
     './src/index'
   ],
   output: {
@@ -13,9 +11,6 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: 'static/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
   module: {
     loaders: [{
       test: /\.js$/,
